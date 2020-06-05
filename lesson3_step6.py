be6389d74f78b1f7dda272d02d063e7fa9741fd5
serGAY921 <firnis8@gmail.com>
2020-06-05 18:03:19 +0700

    Добавил блок, который находит кнопку и кликает по ней

--- lesson3_step6.py
+++ lesson3_step6.py
@@ -7,10 +7,13 @@ browser = webdriver.Chrome()
 browser.get(link)
 
 try:
-        
+    button1 = browser.find_element_by_css_selector("button.btn")
+    button1.click()
+
+    time.sleep(1)
 
 finally:
-        # ожидание чтобы визуально оценить результаты прохождения скрипта
-        time.sleep(10)
-        # закрываем браузер после всех манипуляций
-        browser.quit()
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
