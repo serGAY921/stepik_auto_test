66cb4097df3d9af67575b39f8ece1dae25e47ea3
serGAY921 <firnis8@gmail.com>
2020-06-05 18:07:16 +0700

    Добавил блок для нахождения значения

--- lesson3_step6.py
+++ lesson3_step6.py
@@ -15,6 +15,10 @@ try:
     new_window = browser.window_handles[1]
     browser.switch_to.window(new_window)
 
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+
 finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
     time.sleep(10)
