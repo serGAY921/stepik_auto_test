17358476bc253f4d900e26657ec9cd32deea09bb
serGAY921 <firnis8@gmail.com>
2020-06-05 18:09:16 +0700

    Добавил функуию для вычесления значения

--- lesson3_step6.py
+++ lesson3_step6.py
@@ -2,6 +2,10 @@ from selenium import webdriver
 import time
 import math
 
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+
 link = "http://suninjuly.github.io/redirect_accept.html"
 browser = webdriver.Chrome()
 browser.get(link)
