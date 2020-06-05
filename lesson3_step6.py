cd7d4ebddb3fac1e38de9778afe39c92b1a16a23
serGAY921 <firnis8@gmail.com>
2020-06-05 18:01:26 +0700

    Добавил блок try ... finally

--- lesson3_step6.py
+++ lesson3_step6.py
@@ -5,3 +5,12 @@ import math
 link = "http://suninjuly.github.io/redirect_accept.html"
 browser = webdriver.Chrome()
 browser.get(link)
+
+try:
+        
+
+finally:
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
